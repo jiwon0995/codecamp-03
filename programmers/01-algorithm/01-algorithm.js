f6201cef7c8d61c892bad61ed218166d31c17d75
jiwon0995 <jiwon0995@gmail.com>
2021-08-31 20:41:26 +0900

    update : algorithm [arrat, object]

---
+++ programmers/01-algorithm/01-algorithm.js
@@ -0,0 +1,30 @@
+//003. 배열의 선언과 할당
+
+// let fruits = ["사과", "바나나", "파인애플"]
+
+//004. 배열의 기능
+
+let fruits = ["사과", "바나나", "파인애플"]
+let newFruits = []
+
+let lastword = fruits[fruits.length - 1]
+
+newFruits = lastword
+
+//008. 객체의 선언과 할당
+
+const students = { "name": "철수" }
+
+//009. 객체의 키&값 추가
+
+const student = {
+  name: "철수",
+  age: 8,
+};
+
+const school = {
+  name: "다람쥐초등학교",
+  teacher: "다람이",
+}
+
+student.school = school;
